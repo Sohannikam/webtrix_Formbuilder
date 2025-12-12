be6af31a4a5af13a60bfd687f62b1fd8a113e613
Sohan Nikam <nikamsohan35@gmail.com>
2025-12-12 23:20:19 +0530

    remove unused import

--- src/Forms/PublicLeadForm.jsx
+++ src/Forms/PublicLeadForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { API_BASE_URL } from '@config/config';
 import { fetchJson } from '@utils/fetchJson';
 
